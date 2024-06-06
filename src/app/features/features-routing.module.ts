170940c2fca96edf6374da7422f7e670be6d999d
Azizul <islamazizul0000@gmail.com>
2024-06-07 00:55:20 +0600

    working on login page

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -4,10 +4,17 @@ import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthenticationGuard } from '../authentication/guards/authentication.guard';
 import { Routes, RouterModule } from '@angular/router';
+import { RoleConstants } from '../authentication/models/user.model';
 
 const routes: Routes = [
-  {path:"home",canActivate: [AuthenticationGuard], component:HomeComponent},
-  {path:"dashboard",canActivate: [AuthenticationGuard],component:DashboardComponent}
+  //{path:"home",canActivate: [AuthenticationGuard], component:HomeComponent},
+  {path:"dashboard",canActivate: [AuthenticationGuard],component:DashboardComponent},
+  {
+    path: 'admin',
+    component: HomeComponent,
+    canActivate: [AuthenticationGuard],
+    data: { roles: [RoleConstants.SuperAdmin] }
+},
 ];
 
 @NgModule({
--- src/app/outside-layout/account/login/login.component.ts
+++ src/app/outside-layout/account/login/login.component.ts
@@ -2,24 +2,21 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../../authentication/authentication.service';
 import { AccountService } from '../account.service';
 import { AlertService } from '../../../Shared/alert.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit{
-  loginForm!: FormGroup;
+  loginForm: FormGroup;
   error: string = "";
   ngOnInit(): void {
-    this.loginForm = new FormGroup({
-      phone: new FormControl("string", Validators.required),
-      password: new FormControl("string", Validators.required)
-});
+
   }
 
  
